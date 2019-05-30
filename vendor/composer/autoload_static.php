c697f1922c0e7ff513fadce6f2752f766d2f49b0
Gomes <gomes.webads@gmail.com>
2019-05-30 11:12:45 -0300

    Correção de vendor name para autoload

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,8 +6,18 @@ namespace Composer\Autoload;
 
 class ComposerStaticInitf81a3432dd3a4559256b41876de75493
 {
-    public static $fallbackDirsPsr4 = array (
-        0 => __DIR__ . '/..' . '/hcodebr/php-classes/src',
+    public static $prefixLengthsPsr4 = array (
+        'H' => 
+        array (
+            'Hcode\\' => 6,
+        ),
+    );
+
+    public static $prefixDirsPsr4 = array (
+        'Hcode\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/hcodebr/php-classes/src',
+        ),
     );
 
     public static $prefixesPsr0 = array (
@@ -41,7 +51,8 @@ class ComposerStaticInitf81a3432dd3a4559256b41876de75493
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->fallbackDirsPsr4 = ComposerStaticInitf81a3432dd3a4559256b41876de75493::$fallbackDirsPsr4;
+            $loader->prefixLengthsPsr4 = ComposerStaticInitf81a3432dd3a4559256b41876de75493::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInitf81a3432dd3a4559256b41876de75493::$prefixDirsPsr4;
             $loader->prefixesPsr0 = ComposerStaticInitf81a3432dd3a4559256b41876de75493::$prefixesPsr0;
             $loader->classMap = ComposerStaticInitf81a3432dd3a4559256b41876de75493::$classMap;
 

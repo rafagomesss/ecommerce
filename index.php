ea01a4536f086b1355f1f531124f1161a1ab69bb
Gomes <gomes.webads@gmail.com>
2019-05-30 10:51:34 -0300

    Remoção de arquivos. Limpeza de código arquivo index

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 require_once("vendor/autoload.php");
 
@@ -7,11 +7,9 @@ $app = new \Slim\Slim();
 $app->config('debug', true);
 
 $app->get('/', function() {
-    
+
 	echo "OK";
 
 });
 
 $app->run();
-
- ?>
